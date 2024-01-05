300bd00b05627f5edef1e3416069c3e48b1862a1
MikaCat2008 <dimalogat@gmail.com>
2024-01-05 13:56:02 +0200

    + tests control blocks

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ import unittest
 from pygame.display import set_mode
 
 from scratch_api import set_screen
+
 from scratch_api.tests.motion import MotionTestCase as t1
+from scratch_api.tests.control import ControlTestCase as t2
 
 set_screen(set_mode((32, 32)))
 
