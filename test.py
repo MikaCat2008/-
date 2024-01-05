d789a6b08690dcaf9e25f2783e99e8489b7a5384
MikaCat2008 <dimalogat@gmail.com>
2024-01-05 18:53:28 +0200

    + test data

--- test.py
+++ test.py
@@ -6,9 +6,10 @@ from scratch_api import set_screen
 
 from scratch_api.tests.nodes import NodesTestCase as t1
 from scratch_api.tests.motion import MotionTestCase as t2
-from scratch_api.tests.events import EventsTestCase as t3
-from scratch_api.tests.control import ControlTestCase as t4
-from scratch_api.tests.operators import OperatorsTestCase as t5
+from scratch_api.tests.data import DataTestCase as t3
+from scratch_api.tests.events import EventsTestCase as t4
+from scratch_api.tests.control import ControlTestCase as t5
+from scratch_api.tests.operators import OperatorsTestCase as t6
 
 set_screen(set_mode((32, 32)))
 
