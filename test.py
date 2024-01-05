60e0b5f972cc509ce6afcb4b3caff02707b4ea0b
MikaCat2008 <dimalogat@gmail.com>
2024-01-05 14:56:01 +0200

    + tests for operators

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from scratch_api import set_screen
 
 from scratch_api.tests.motion import MotionTestCase as t1
 from scratch_api.tests.control import ControlTestCase as t2
+from scratch_api.tests.operators import OperatorsTestCase as t3
 
 set_screen(set_mode((32, 32)))
 
