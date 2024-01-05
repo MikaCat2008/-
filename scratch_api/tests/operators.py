d7147f74540c18e74602b5e292d718339d57c62a
MikaCat2008 <dimalogat@gmail.com>
2024-01-05 16:19:17 +0200

    + tests for random number, less than, equals to, bigger than, and, or, not, math func of node

--- scratch_api/tests/operators.py
+++ scratch_api/tests/operators.py
@@ -49,3 +49,61 @@ class OperatorsTestCase(unittest.TestCase):
 
         self.assertEqual(node1.get_value(), 0.25)
         self.assertEqual(node2.get_value(), 0.5)
+
+    def test_random_number_node(self) -> None:
+        node1 = RandomNumberNode(1, 10)
+        node2 = RandomNumberNode(-10, -1)
+        node3 = RandomNumberNode(0, 1)
+
+        self.assertIn(node1.get_value(), range(1, 11))
+        self.assertIn(node2.get_value(), range(-10, -1))
+        self.assertIn(node3.get_value(), range(2))
+
+    def test_less_than_node(self) -> None:
+        node1 = LessThanNode(1, 2)
+        node2 = LessThanNode(2, 1)
+
+        self.assertTrue(node1.get_value())
+        self.assertFalse(node2.get_value())
+
+    def test_equals_to_node(self) -> None:
+        node1 = EqualsToNode(1, 1)
+        node2 = EqualsToNode(1, 2)
+
+        self.assertTrue(node1.get_value())
+        self.assertFalse(node2.get_value())
+
+    def test_bigger_than_node(self) -> None:
+        node1 = BiggerThanNode(2, 1)
+        node2 = BiggerThanNode(1, 2)
+
+        self.assertTrue(node1.get_value())
+        self.assertFalse(node2.get_value())
+
+    def test_and_node(self) -> None:
+        node1 = AndNode(True, True)
+        node2 = AndNode(True, False)
+
+        self.assertTrue(node1.get_value())
+        self.assertFalse(node2.get_value()) 
+
+    def test_or_node(self) -> None:
+        node1 = OrNode(True, False)
+        node2 = OrNode(False, False)
+
+        self.assertTrue(node1.get_value())
+        self.assertFalse(node2.get_value()) 
+
+    def test_not_node(self) -> None:
+        node1 = NotNode(False)
+        node2 = NotNode(True)
+
+        self.assertTrue(node1.get_value())
+        self.assertFalse(node2.get_value()) 
+
+    def test_math_func_of_node(self) -> None:
+        node1 = MathFuncOfNode("sin", 45 * math.pi / 180)
+        node2 = MathFuncOfNode("cos", 45 * math.pi / 180)
+
+        self.assertAlmostEqual(node1.get_value(), math.sin(45 * math.pi / 180))
+        self.assertAlmostEqual(node2.get_value(), math.cos(45 * math.pi / 180))
