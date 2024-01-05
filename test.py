54823802e5f12cc55f587f3a0f1ec1154a1e871c
MikaCat2008 <dimalogat@gmail.com>
2024-01-05 17:48:52 +0200

    + test events

--- test.py
+++ test.py
@@ -5,8 +5,9 @@ from pygame.display import set_mode
 from scratch_api import set_screen
 
 from scratch_api.tests.motion import MotionTestCase as t1
-from scratch_api.tests.control import ControlTestCase as t2
-from scratch_api.tests.operators import OperatorsTestCase as t3
+from scratch_api.tests.events import EventsTestCase as t2
+from scratch_api.tests.control import ControlTestCase as t3
+from scratch_api.tests.operators import OperatorsTestCase as t4
 
 set_screen(set_mode((32, 32)))
 
