a1e909b8af7ce766d40022e234baf14a55568a02
MikaCat2008 <dimalogat@gmail.com>
2024-01-06 19:39:03 +0200

    + if on edge bounce block

---
+++ scratch_api/motion/IfOnEdgeBounceBlock.py
@@ -0,0 +1,27 @@
+from ..block import Block
+from ..memory import memory
+
+
+class IfOnEdgeBounceBlock(Block):
+    def __init__(self, *args: tuple) -> None:
+        super().__init__(())
+
+    def execute(self) -> bool:
+        screen_w, screen_h = memory.screen.get_size()
+        sprite_x, sprite_y = self.sprite.coords
+        sprite_w, sprite_h = self.sprite.rendered_surface.get_size()
+
+        direction = self.sprite.direction
+
+        if sprite_x - sprite_w / 2 <= 0 or sprite_x + sprite_w / 2 >= screen_w:
+            if 0 <= direction <= 180:
+                direction = 180 - direction
+            elif 180 <= direction <= 360:
+                direction = 540 - direction
+        elif sprite_y - sprite_h / 2 <= 0 or sprite_y + sprite_h / 2 >= screen_h:
+            direction = 360 - direction
+
+        if direction != self.sprite.direction:
+            self.sprite.set_direction(direction)
+
+        return True
