22fd9fbecfac5e2335b2e83c8808129d4291c336
MikaCat2008 <dimalogat@gmail.com>
2024-01-06 17:05:31 +0200

    + point in direction block

---
+++ scratch_api/motion/PointInDirectionBlock.py
@@ -0,0 +1,16 @@
+from ..abstractions import Number
+from ..block import Block
+
+
+class PointInDirectionBlock(Block):
+    direction: Number
+
+    def __init__(self, *args: tuple[Number]) -> None:
+        super().__init__(args)
+
+        self.direction = args[0]
+
+    def execute(self) -> bool:
+        self.sprite.direction = float(self.direction)
+
+        return True
