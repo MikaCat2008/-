973b6e2529ce31e79a4dea8f415c10744e4a6f9f
MikaCat2008 <dimalogat@gmail.com>
2024-01-08 15:40:04 +0200

    + nodes in init

--- scratch_api/node.py
+++ scratch_api/node.py
@@ -5,6 +5,8 @@ class Node(NodeType):
     def __init__(self) -> None:
         super().__init__()
 
+        self.nodes = []
+
     def init(self, sprite: SpriteType) -> None:
         self.nodes = [node for _, node in self.__dict__.items() if isinstance(node, NodeType)]
         
