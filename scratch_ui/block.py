b31bb497bdc23a9590c3f5a3337fbbf15df4a5b2
MikaCat2008 <dimalogat@gmail.com>
2024-01-14 22:17:48 +0200

    + get_template_element_by_y, get_slot_by_coords, is_event, is_iterable methods

--- scratch_ui/block.py
+++ scratch_ui/block.py
@@ -1,6 +1,7 @@
 from pygame.surface import SurfaceType
 
-from .abstractions import BlockType, SpriteType, BlockSlotType
+from .abstractions import BlockType, SpriteType, BlockSlotType, TemplateElementType
+from .template_elements import BlocksTemplateElement
 
 from scratch_api.block import BlockType as GameBlockType
 
@@ -16,6 +17,7 @@ class Block(BlockType):
         self.coords = coords
         self.game_block = game_block
         self.rendered = None
+        self.slot = None
 
         self.slots = []
         self.template = None
@@ -28,12 +30,30 @@ class Block(BlockType):
         block.init()
 
         return block
-    
+
+    def get_template_element_by_y(self, y: int) -> tuple[TemplateElementType, int, int]:        
+        h = 0
+        for template_element in self.template.template_elements:
+            tew, teh = template_element.rendered.get_size()
+
+            if h <= y <= h + teh:
+                return template_element, tew, h
+
+            h += teh
+
+    def get_slot_by_coords(self, x: int, y: int) -> tuple[BlockSlotType, int, int]:
+        template_element, w, h = self.get_template_element_by_y(y)
+
+        if isinstance(template_element, BlocksTemplateElement):
+            if 20 <= x:
+                return template_element.slot, w, h
+        return None, None, None
+
     def get_child(self, mx: int, my: int) -> tuple[BlockType, int, int]:
         for slot in self.slots:
             y = 25
             if isinstance(slot, BlockSlotType):
-                for i, block in enumerate(slot.blocks):
+                for block in slot.blocks:
                     bx, by = block.coords
                     bw, bh = block.rendered.get_size()
 
@@ -48,6 +68,12 @@ class Block(BlockType):
                     y += bh
 
         return self, 0, 0
+
+    def is_event(self) -> bool:
+        return not self.game_block.event is None
+
+    def is_iterable(self) -> bool:
+        return any(isinstance(slot, BlockSlotType) for slot in self.slots)
 
     def render(self) -> SurfaceType:
         self.rendered = self.template.render()
