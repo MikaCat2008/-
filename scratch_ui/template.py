105dbb67c4229c0512344cf1a25d24707e088628
MikaCat2008 <dimalogat@gmail.com>
2024-01-27 15:11:38 +0200

    template for nodes

--- scratch_ui/template.py
+++ scratch_ui/template.py
@@ -8,45 +8,62 @@ class Template(TemplateType):
     def __init__(
         self, 
         template_elements: list[TemplateElementType], 
-        color: tuple[int, int, int]
+        color: tuple[int, int, int],
+        mode: str = "block"
     ) -> None:
         self.width = None
         self.template_elements = template_elements
+        self.color = color
+        self.mode = mode
 
         for template_element in template_elements:
-            template_element.color = color
             template_element.template = self
 
     def get_surfaces(self) -> list[SurfaceType]:
-        surfaces = [None] * len(self.template_elements)
+        if self.mode == "block":
+            surfaces = [None] * len(self.template_elements)
 
-        for i, template_element in enumerate(self.template_elements):
-            if i == 0:
-                surface = template_element.render()
-            else:
-                surface = template_element.render(surfaces[i - 1].get_size()[1])
+            for i, template_element in enumerate(self.template_elements):
+                if i == 0:
+                    surface = template_element.render()
+                else:
+                    surface = template_element.render(surfaces[i - 1].get_size()[1])
 
-            surfaces[i] = surface
-            template_element.rendered = surface
+                surfaces[i] = surface
+                template_element.rendered = surface
+        else:
+            surfaces = self.template_elements[0].get_surfaces()
 
         return surfaces
 
     def render(self) -> SurfaceType:
         surfaces = self.get_surfaces()
         
-        sw, sh = 0, 0
-        for _surface in surfaces:
-            w, h = _surface.get_size()
-            
-            sw = max(w, sw)
-            sh += h
-        
-        surface = Surface((sw, sh), SRCALPHA, 32)
-        
-        y = 0
-        for _surface in surfaces:
-            surface.blit(_surface, (0, y))
+        sw, sh, w, y = 0, 0, 0, 0
 
-            y += _surface.get_size()[1]
+        if self.mode == "block":
+            for _surface in surfaces:
+                w, h = _surface.get_size()
+                
+                sw = max(w, sw)
+                sh += h
+            
+            surface = Surface((sw, sh), SRCALPHA, 32)
+            
+            for _surface in surfaces:
+                surface.blit(_surface, (0, y))
+
+                y += _surface.get_height()
+        else:
+            for _surface in surfaces:
+                sw += _surface.get_width()
+
+            surface = Surface((sw, 23), SRCALPHA, 32)
+            surface.fill(self.color)
+
+            for _surface in surfaces:
+                surface.blit(_surface, (w, 0))
+
+                w += _surface.get_width()
 
         return surface
