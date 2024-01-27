797fc5252e391438b13ff19f30d46e2eded0a468
MikaCat2008 <dimalogat@gmail.com>
2024-01-27 15:07:30 +0200

    + select manager

---
+++ scratch_ui/select_manager.py
@@ -0,0 +1,28 @@
+import inspect
+
+from .abstractions import NodeType, BlockType, SelectManagerType, SelectableObjectType
+
+
+class SelectManager(SelectManagerType):
+    def __init__(self) -> None:
+        self.selected_object = None
+
+    def select(self, selectable_object: SelectableObjectType) -> None:
+        self.selected_object = selectable_object
+
+    def unselect(self) -> None:
+        self.selected_object = None
+
+    def get_node(self) -> None | NodeType:
+        if isinstance(self.selected_object, NodeType):
+            return self.selected_object
+
+    def get_block(self) -> None | BlockType:
+        if isinstance(self.selected_object, BlockType):
+            return self.selected_object
+
+    def free(self) -> None:
+        self.selected_object.remove()
+
+
+select_manager = SelectManager()
