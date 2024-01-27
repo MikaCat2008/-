1dfeb1a76d9a15966ff38c0172e23f8a2f9b3dee
MikaCat2008 <dimalogat@gmail.com>
2024-01-27 15:10:56 +0200

    some text rendering changes

--- scratch_ui/font.py
+++ scratch_ui/font.py
@@ -1,10 +1,23 @@
+from pygame import SRCALPHA
 from pygame.font import init as init_font, SysFont
-from pygame.surface import SurfaceType
+from pygame.surface import Surface, SurfaceType
 
 init_font()
 
 font = SysFont(None, 32)
 
 
-def text_render(text: str, color: tuple[int, int, int] = (255, 255, 255)) -> SurfaceType:
-    return font.render(text, None, color)
+def text_render(
+    text: str, 
+    color: tuple[int, int, int] = (255, 255, 255),
+    background_color: tuple[int, int, int, int] = (0, 0, 0, 0),
+    indent: int = 0
+) -> SurfaceType:
+    text = font.render(text, None, color)
+
+    w = text.get_width()
+    surface = Surface((w + indent * 2, 23), SRCALPHA, 32)
+    surface.fill(background_color)
+    surface.blit(text, (indent, 0))
+
+    return surface
