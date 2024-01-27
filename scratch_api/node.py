24bde8605a75fdd23b796423c4b50a7711df8e7c
MikaCat2008 <dimalogat@gmail.com>
2024-01-27 15:05:24 +0200

    + parent_node

--- scratch_api/node.py
+++ scratch_api/node.py
@@ -6,15 +6,21 @@ class Node(NodeType):
         super().__init__()
 
         self.nodes = []
+        self.parent_node = None
 
     def init(self, sprite: SpriteType) -> None:
         self.nodes = [node for _, node in self.__dict__.items() if isinstance(node, NodeType)]
         
         for node in self.nodes:
             node.init(sprite)
+            node.parent_node = self
         
         self.sprite = sprite
 
     def reset(self) -> None:
         for node in self.nodes:
             node.reset()
+
+    def remove(self) -> None:
+        # print(self.parent_node)
+        ...
